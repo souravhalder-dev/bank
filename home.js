91ae13e14a094b0ee46ba60075ff32c95d39896b
Skhsouravhalder <skhsouravhalder@gmail.com>
2025-08-23 20:39:52 +0600

    update togle

--- home.js
+++ home.js
@@ -1,14 +1,47 @@
+const validPin = 1234;
+
 document.getElementById("add-money").addEventListener("click", function (e) {
   e.preventDefault();
   const bankName = document.getElementById("addbank").value;
   const accountNumber = document.getElementById("add-accountnumber").value;
   const addAmmount = parseInt(document.getElementById("add-ammount").value);
-  const pinNumber = document.getElementById("pin-number").value;
-  //   console.log(bankName, accountNumber, addAmmount, pinNumber);
+  const pinNumber = parseInt(document.getElementById("pin-number").value);
+
+  if (accountNumber.length < 11) {
+    alert("Invalid account Number");
+    return;
+  }
+
+  if (pinNumber !== validPin) {
+    alert("Invalid pin Number");
+    return;
+  }
   const availableBalance = parseInt(
     document.getElementById("available-balance").innerText
   );
   const total = addAmmount + availableBalance;
 
   document.getElementById("available-balance").innerText = total;
+});
+
+// toggle
+document.getElementById("addmoney-tog").addEventListener("click", function () {
+  document.getElementById("cashout").style.display = "none";
+  document.getElementById("money").style.display = "block";
+  document.getElementById("bonus").style.display = "none";
+});
+document.getElementById("cashOut-tog").addEventListener("click", function () {
+  document.getElementById("money").style.display = "none";
+  document.getElementById("cashout").style.display = "block";
+});
+document.getElementById("transfer-tog").addEventListener("click", function () {
+  document.getElementById("money").style.display = "none";
+  document.getElementById("cashout").style.display = "none";
+  document.getElementById("transfer").style.display = "block";
+});
+document.getElementById("bonus-tog").addEventListener("click", function () {
+  document.getElementById("money").style.display = "none";
+  document.getElementById("cashout").style.display = "none";
+  document.getElementById("transfer").style.display = "none";
+  document.getElementById("bonus").style.display = "block";
 });
